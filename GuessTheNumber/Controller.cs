01174143296c29d1a619ca9f1ca10d66939e84c4
HienaDev <ant.rodrigues1999@gmail.com>
2023-05-25 21:55:49 +0100

    Add controller and consoleview class

---
+++ GuessTheNumber/Controller.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    public class Controller
+    {
+
+        private int attemps = 0;
+        private int guess;
+
+        public void Run()
+        {
+            bool guessedCorrectly = false;
+
+            while(!guessedCorrectly)
+            {
+                ConsoleView
+            }
+        }
+
+
+    }
+}
